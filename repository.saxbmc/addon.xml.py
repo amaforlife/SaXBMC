caa79e2e11ee4791d45bb6e40dfc0d838209a4a9
b-jesch <birger.jesch@gmail.com>
2015-05-19 17:48:17 +0200

    Repository installer added

---
+++ repository.saxbmc/addon.xml.py
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<addon id="repository.saxbmc" name="SaXBMC Addon Repo" version="1.0.0" provider-name="Birger Jesch">
+	<extension point="xbmc.addon.repository" name="SaXBMC Addon Repo">
+		<info compressed="false">https://raw.githubusercontent.com/b-jesch/SaXBMC/master/addons.xml</info>
+		<checksum>https://raw.githubusercontent.com/b-jesch/SaXBMC/master/addons.xml.md5</checksum>
+		<datadir zip="true">https://raw.githubusercontent.com/b-jesch/SaXBMC/master/zip/</datadir>
+	</extension>
+	<extension point="xbmc.addon.metadata">
+		<summary>Some useful services and scripts</summary>
+		<description>Some useful services and scripts</description>
+		<disclaimer></disclaimer>
+		<platform>all</platform>
+	</extension>
+</addon>
