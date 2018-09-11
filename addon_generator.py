d3e77ed1d72c33a93015ea677873d2bd5b73858d
b-jesch <birger.jesch@gmail.com>
2018-09-11 09:08:55 +0200

    introducing script.input.adsp

--- addon_generator.py
+++ addon_generator.py
@@ -33,7 +33,7 @@ import hashlib
 from xml.dom import minidom
 
 MY_ADDONS = ['plugin.video.ipcams', 'repository.saxbmc', 'script.helper.bumblebox', 'script.homematic.sonoff',
-             'script.loungeripper', 'script.module.oauth2client', 'script.program.driverselect',
+             'script.loungeripper', 'script.module.oauth2client', 'script.program.driverselect', 'script.input.adsp',
              'script.program.fritzact', 'script.service.caretaker', 'script.service.gto', 'service.calendar',
              'service.fritzbox.callmonitor', 'service.kn.switchtimer', 'service.lgtv.remote', 'service.librespot',
              'service.pvr.manager', 'service.sleepy.watchdog',
