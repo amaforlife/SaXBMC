d32d7c51e2286c658ba74b1cb2eeac160c5bff64
b-jesch <birger.jesch@gmail.com>
2019-05-22 10:28:11 +0200

    script.video.binge added

--- addon_generator.py
+++ addon_generator.py
@@ -48,9 +48,9 @@ repository itself should be included in this list.
 
 MY_ADDONS = ['plugin.video.ipcams', 'repository.saxbmc', 'script.helper.bumblebox', 'script.homematic.sonoff',
              'script.loungeripper', 'script.module.oauth2client', 'script.program.driverselect', 'script.input.adsp',
-             'script.program.fritzact', 'script.service.caretaker', 'script.service.gto', 'service.calendar',
-             'service.fritzbox.callmonitor', 'service.kn.switchtimer', 'service.lgtv.remote', 'service.librespot',
-             'service.pvr.manager', 'service.sleepy.watchdog',
+             'script.program.fritzact', 'script.service.caretaker', 'script.service.gto', 'script.video.binge',
+             'service.calendar', 'service.fritzbox.callmonitor', 'service.kn.switchtimer', 'service.lgtv.remote',
+             'service.librespot', 'service.pvr.manager', 'service.sleepy.watchdog',
              ]
 
 '''
