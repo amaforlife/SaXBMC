8719b32d13a2df2fb540bea825bd55e2c85eacc0
b-jesch <birger.jesch@gmail.com>
2019-06-26 08:29:59 +0200

    cosmetics

--- addon_generator.py
+++ addon_generator.py
@@ -46,9 +46,11 @@ your addons which should deployed to your repository on github. Modify the list 
 repository itself should be included in this list.
 '''
 
+# 'script.service.caretaker' temporary removed
+
 MY_ADDONS = ['plugin.video.ipcams', 'repository.saxbmc', 'script.helper.bumblebox', 'script.homematic.sonoff',
              'script.loungeripper', 'script.module.oauth2client', 'script.program.driverselect', 'script.input.adsp',
-             'script.program.fritzact', 'script.service.caretaker', 'script.service.gto', 'script.video.binge',
+             'script.program.fritzact', 'script.service.gto', 'script.video.binge',
              'service.calendar', 'service.fritzbox.callmonitor', 'service.kn.switchtimer', 'service.lgtv.remote',
              'service.librespot', 'service.pvr.manager', 'service.sleepy.watchdog',
              ]
